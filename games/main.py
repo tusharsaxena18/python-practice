3e61a386e8064af469cbe9ff61b97cff739d9b35
Tushar Saxena <tusharsaxena1203@gmail.com>
2023-07-29 19:06:50 +0530

    wee

--- games/main.py
+++ games/main.py
@@ -3,21 +3,32 @@ import pygame
 pygame.init()
 
 WIDTH, HEIGHT = 300, 400
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT) ,
+                                 pygame.RESIZABLE)
+img = pygame.image.load("D:\code\python-practice\games\Vector.png")
 pygame.display.set_caption("Tic Tac Toe")
+pygame.display.set_icon(img)
 clock = pygame.time.Clock()
 
+
 def main(): 
+    global color
+    color = "red"
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
     
-
-    pygame.display.flip() 
+        screen.fill(color)
+        pygame.display.flip() 
+        if color == "red":
+            color = "blue"
+        else:
+            color = "red"
+        
+        clock.tick(60) # Limits the fps to 60 
     pygame.quit()
-    clock.tick(60) # Limits the fps to 60 
 
 if __name__ == "__main__": # main loop
     main() 
---
+++ node js/index.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+
+for (let i = 0; i < 12 ; i++) {
+    return         
+}
